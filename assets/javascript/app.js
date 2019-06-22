b46fdd2fb52a298f3d4c48ccd6593eeade2d1277
Richard Zhou (Insight Global Inc) <v-riczho@microsoft.com>
2019-06-22 00:41:19 -0700

    cleaned up script, updated css/fonts and styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,14 @@
 var correct = 0;
 var wrong = 0;
 
-//current question to store from loop
 var currentQ = 0;
-//holds input from user
 var nAnswered = 0;
 var answered = 0;
 var userSelect = 0;
 
-//timer variables
 var sec = 0;
 var time = 0;
 
-//messages for new screen after answer result 
 var messages = {
     correct: "Correct!",
     incorrect: "Sorry, that's incorrect!",
@@ -21,32 +17,26 @@ var messages = {
 }
 
 var Questions = [{
-    //Question One:
     question: "What's the average temperature of lightning?",
     answerList: ["36,000 °F", "100 °F", "Same as a car", "Not as much as my laptop"],
     answer: 0
 }, {
-    //Question Two:
     question: "In 1995, Texas had hailstones as big as Softballs.",
     answerList: ["True", "False"],
     answer: 0
 }, {
-    //Question Three:
     question: "What is hail and how do they form?",
     answerList: ["Small droplets that are caught in the updraft", "Small baseballs thrown at us by the sky", "Free ice", "I thought they were rocks"],
     answer: 0
 }, {
-    //Question Four:
     question: "How do tornadoes form?",
     answerList: ["Warm, moist air + Cool, dry air", "When the sky is angry", "Strong winds", "When I Sneeze"],
     answer: 0
 }, {
-    //Question Five:
     question: "The lowest natural temperature ever at ground level on Earth is -128.6 °F at the Soviet Vostok Station in Antarctica.",
     answerList: ["True", "False"],
     answer: 0
 }, {
-    //Question Six:
     question: "Which of the following is a common cause for floods?",
     answerList: ["Heavy Rain", "When the ground doesn't absorb water fast enough", "When water isn't being used", "When the neighbor's toilet cloggs"],
     answer: 0
@@ -59,19 +49,17 @@ $(document).ready(function () {
     $("#resetButton").hide()
     $("#answerImage").hide()
     $("#timer").hide()
+    $("#currentQ").hide()
     function Game() {
-        //clear html
         $("#message").empty();
         $("#correct").empty();
         $("#wrong").empty();
         $("#nAnswered").empty();
         $("#timer").show();
-        //clear counter
         currentQ = 0;
         correct = 0;
         wrong = 0;
         nAnswered = 0;
-        //call to generate first question 
         newQuestion()
     }
 
@@ -80,9 +68,9 @@ $(document).ready(function () {
         $("#intropage").empty()
         $("#correctAnswer").empty();
         $("#answerImage").hide()
+        $("#currentQ").show()
         answered = true;
 
-        //sets up new question
         $("#currentQ").text("Question #" + (currentQ + 1) + " of " + Questions.length);
         $("#question").text(Questions[currentQ].question);
         for (var i = 0; i < 4; i++) {
@@ -92,10 +80,8 @@ $(document).ready(function () {
             choices.addClass("choiceList");
             $(".answerChoices").append(choices);
         }
-        //timer
         countDown();
 
-        //clicking an answer will pause the time and setup answerPage
         $(".choiceList").on("click", function () {
             userSelect = $(this).data('index');
             clearInterval(time);
@@ -104,36 +90,25 @@ $(document).ready(function () {
     }
 
     function answerPage() {
-        //Clears question page
-        $("#currentQ").empty();
+        $("#currentQ").hide();
         $(".choiceList").empty();
         $(".question").empty();
         $("#answerImage").show()
-        //holds the place for answer
         var correctAnswerText = Questions[currentQ].answerList[Questions[currentQ].answer];
-        //correct answer place in array
         var correctAnswerIndex = Questions[currentQ].answer;
 
-        //checks to see correct, wrong, or unanswered
-        //if player chooses the right answer 
         if ((userSelect == correctAnswerIndex) && (answered == true)) {
-            //then wins increase by one
             correct++;
-            //and the correct message displays on new screen
             $("#status").html(messages.correct);
-            //if player chooses wrong answer
             console.log("Question: " + Questions[currentQ].question)
             checkImg()
         } else if ((userSelect != correctAnswerIndex) && (answered == true)) {
-            //wrong answer count goes up by one
             wrong++;
             $("#status").html(messages.incorrect);
-            //correct answer displays
             $("#correctAnswer").html("The correct answer was: " + correctAnswerText);
             console.log("Question: " + Questions[currentQ].question)
             checkImg()
         } else {
-            //if player does not choose one before the timer runs out
             nAnswered++;
             $("#status").html(messages.endTime);
             $("#correctAnswer").html("The correct answer was: " + correctAnswerText);
@@ -141,11 +116,9 @@ $(document).ready(function () {
             answered = true;
             checkImg()
         }
-        //once the last question is complete display scoreboard
         if (currentQ == (Questions.length - 1)) {
             setTimeout(finalScreen, 4000)
-        } else {
-            //otherwise, display next question 
+        } else { 
             currentQ++;
             setTimeout(newQuestion, 4000);
             
@@ -170,56 +143,42 @@ $(document).ready(function () {
     }
 
     function finalScreen() {
-        //clear the timer, and correctedAnswer 
         $("#timer").empty();
         $("#status").empty();
         $("#correctAnswer").empty();
         $("#answerImage").hide()
         $("#question").empty();
-        //display message 
         $("#message").html(messages.finished);
-        //final counter results
         $("#correct").html("You got " + correct + " correct!");
         $("#wrong").html("You got " + wrong + " wrong!");
         $("#nAnswered").html("Unanswered questions: " + nAnswered);
-        //reset game 
         $("#resetButton").show();
     }
 
 
-    //PROCESS
-    //___________________________________________
-
-    //start button 
     $("#button").on("click", function () {
         $(this).hide();
         Game();
     });
-    //reset button
     $("#resetButton").on("click", function () {
         $(this).hide();
         Game();
     });
 
     function countDown() {
-        //seconds per question
         sec = 10;
         $("#timer").text("Time Left: " + sec);
         answered = true;
-        //sets timer to go down
         time = setInterval(showCountdDown, 1000);
     }
 
 
     function showCountdDown() {
-        //seconds countdown
         sec--;
         $("#timer").text("Time Left: " + sec);
-        //if seconds left is less than 1, then clear timer as player ran out of time 
         if (sec < 1) {
             clearInterval(time);
             answered = false;
-            //also displays answerPage function, to let player know they ran out of time for the question
             answerPage()
         }
     }
